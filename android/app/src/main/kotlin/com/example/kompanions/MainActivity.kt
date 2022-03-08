2854502df4bd00acf48ec0adfff9c3b930cb34d1
fabien lacorre <fab.lacorre@gmail.com>
2022-03-08 13:39:57 +0100

    feat: start of rework kompanion with react

--- android/app/src/main/kotlin/com/example/kompanions/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.kompanions
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/Classes/Race.dart
+++
@@ -1,9 +0,0 @@
-class Race {
-  String name;
-
-  Race({this.name = ""});
-
-  factory Race.fromJson(Map<String, dynamic> json) => Race(
-        name: json["name"] != null ? json["name"] : "",
-      );
-}
