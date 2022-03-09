39270dcd7a6a0da9ab73929e48b24936a85e6509
fabien lacorre <fab.lacorre@gmail.com>
2022-03-09 12:19:20 +0100

    feat: add some card in dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,28 @@
-const Login = () => {
+import Card from "../components/Card";
+
+const Dashboard = () => {
   return (
     <div className="dashboard-body">
       <h1>Dashboard</h1>
+      <div className="row-display">
+        <Card></Card>
+        <div className="small-right-margin"></div>
+        <Card></Card>
+      </div>
+      <div className="small-separator"></div>
+      <div className="row-display">
+        <Card></Card>
+        <div className="small-right-margin"></div>
+        <Card></Card>
+      </div>
+      <div className="small-separator"></div>
+      <div className="row-display">
+        <Card></Card>
+        <div className="small-right-margin"></div>
+        <Card></Card>
+      </div>
     </div>
   );
 };
 
-export default Login;
+export default Dashboard;
