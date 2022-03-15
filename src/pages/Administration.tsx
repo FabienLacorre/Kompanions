27254165496035c43dcbaf96dc9bce1870cd376f
fabien lacorre <fab.lacorre@gmail.com>
2022-03-15 18:03:09 +0100

    feat: add all users display in admin page

--- src/pages/Administration.tsx
+++ src/pages/Administration.tsx
@@ -3,8 +3,9 @@ import Button from "../components/Button";
 import { addRaceRequest } from "../request/races";
 import Input from "../components/Input";
 import { raceRequest } from "../request/races";
+import { getAllUsersRequest } from "../request/user";
 
-const Administration = () => {
+const RaceManagement = () => {
   const [newRace, setNewRace] = useState("");
   const [allRace, setAllRaces] = useState([]);
 
@@ -24,39 +25,85 @@ const Administration = () => {
   }, []);
 
   return (
+    <div className="column-display">
+      <span className="bold">RACE</span>
+      <div className="small-separator" />
+      <Input placeholder="New race" value={newRace} change={setNewRace} />
+      <div className="small-separator" />
+      <Button click={() => addRace(newRace)}>Submit new race</Button>
+      <div className="small-separator" />
+      <table>
+        <thead>
+          <tr style={{ textAlign: "left" }}>
+            <th>Nom</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {allRace.map((e: any, index: number) => {
+            return (
+              <tr key={`TABLE_RACE_${index}`}>
+                <td>{e.name}</td>
+                <td>
+                  <Button>Remove</Button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const UserManagement = () => {
+  const [allUsers, setAllUsers] = useState([]);
+
+  const getAllUsers = async () => {
+    const resp = await getAllUsersRequest();
+    setAllUsers(resp.data);
+  };
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  return (
+    <div className="column-display">
+      <span className="bold">USERS</span>
+      <div className="small-separator" />{" "}
+      <table>
+        <thead>
+          <tr style={{ textAlign: "left" }}>
+            <th>email</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {allUsers.map((e: any, index: number) => {
+            return (
+              <tr key={`TABLE_USER_${index}`}>
+                <td>{e.email}</td>
+                <td>
+                  <Button>Remove</Button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const Administration = () => {
+  return (
     <div className="administration-body animated-page">
       <h1>Administration</h1>
-
       <hr />
-      {/* // RACE MANAGEMENT // */}
-      <div className="column-display">
-        <span className="bold">RACE</span>
-        <div className="small-separator" />
-        <Input placeholder="New race" value={newRace} change={setNewRace} />
-        <div className="small-separator" />
-        <Button click={() => addRace(newRace)}>Submit new race</Button>
-        <div className="small-separator" />
-        <table>
-          <thead>
-            <tr style={{ textAlign: "left" }}>
-              <th>Nom</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {allRace.map((e: any, index: number) => {
-              return (
-                <tr key={`TABLE_RACE_${index}`}>
-                  <td>{e.name}</td>
-                  <td>
-                    <Button>Remove</Button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+      <UserManagement />
+      <hr />
+      <RaceManagement />
       <hr />
     </div>
   );
--- src/request/user.ts
+++ src/request/user.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
+import { isAsExpression } from "typescript";
 
 const url = "http://192.168.1.129:8090/user";
 
 const saveToken = (token: string) => {
   localStorage.setItem("token", token);
 };
+
 const getToken = () => {
   return localStorage.getItem("token");
 };
@@ -13,4 +15,10 @@ const loginRequest = (email: string, password: string) => {
   return axios.post(`${url}/connect`, { email, password });
 };
 
-export { saveToken, getToken, loginRequest };
+const getAllUsersRequest = () => {
+  return axios.get(`${url}`, {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  });
+};
+
+export { saveToken, getToken, loginRequest, getAllUsersRequest };
